6829943b1fed3969cbfdda29de2b282806a19beb
Pham Quang Binh <binhpq@topcv.vn>
2025-02-28 20:54:58 +0700

    init

---
+++ bhn-wp-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: BHN WP Plugin
+Plugin URI: https://banhangnhanh.vn
+Description: Plugin tùy chỉnh cho WordPress - Thêm tính năng đặc biệt vào trang web của bạn.
+Version: 1.0.0
+Author: BHN
+Author URI: https://banhangnhanh.vn
+*/
+
+if (!defined('ABSPATH')) {
+  exit;
+}
